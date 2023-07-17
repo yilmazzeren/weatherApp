c18a2b27e97afac351a6b50af158fc471f6272e4
yzeren <yilmaz.zeren@pribas.com>
2023-07-17 16:50:12 +0300

    Tabs && location added

--- App.js
+++ App.js
@@ -1,18 +1,47 @@
-import React from 'react'
-import { View, StyleSheet } from 'react-native'
-import UpComingWeather from './src/screens/UpcomingWeather'
-import City from './src/screens/City'
-import CurrentWeather from './src/screens/CurrentWeather'
+import React, { useState, useEffect } from 'react'
+import { ActivityIndicator, StyleSheet, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import Tabs from './src/components/Tabs'
+import * as Location from 'expo-location'
 const App = () => {
+  useEffect(() => {
+    ;(async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setError('permission to access location was denied')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+    })()
+  }, [])
+
+  const [loading, setLoading] = useState(true)
+  const [location, setLocation] = useState(null)
+  const [error, setError] = useState(null)
+
+  if (location) {
+    console.log(location, 'loo')
+  }
+
+  if (!loading) {
+    return (
+      <View style={styles.container}>
+        {/*<ActivityIndicator size={'large'} color={'blue'} />*/}
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <CurrentWeather />
-    </View>
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    justifyContent: 'center',
     flex: 1
   }
 })
--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import RowText from '../components/RowText'
+import { weatherType } from '../utilities/weatherType'
 const CurrentWeather = () => {
   const {
     wrapper,
@@ -30,7 +31,7 @@ const CurrentWeather = () => {
       </View>
       <RowText
         messageOne={'Its sunny'}
-        messageTwo={'Its perfect t-shirt weather'}
+        messageTwo={weatherType.Thunderstorm.message}
         containerStyles={bodyWrapper}
         messageOneStyles={description}
         messageTwoStyles={message}
